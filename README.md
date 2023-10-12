84426a17cafe12a1a4e4b7e6e8ac71fa1fa539fd
Patrick Williams <ptk.williams@gmail.com>
2023-10-12 08:06:45 -0500

    added user and order creation. added support for large numbers of categories on a store.

--- README.md
+++ README.md
@@ -2,22 +2,39 @@
 
 This CLI tool can be used to create fake product data on a Bigcommerce store. It will create a number of products in a new/existing category with prices, images, weights, and descriptions.
 
+It will also create a specified number of users with a specified number of orders using the newly created products. 
+
 This was built 95% with Bing.com/chat and copy/paste.
 
 ## How to use
 
 1. Clone the repo. 
 2. Install dependencies `pip install openai bigcommerce`
-3. Enter your BigCommerce and OpenAI API credentials in the config object.
+3. Enter your [BigCommerce](https://support.bigcommerce.com/s/article/Store-API-Accounts?language=en_US) and [OpenAI](https://help.openai.com/en/articles/4936850-where-do-i-find-my-secret-api-key) API credentials in the config object.
 4. Run the script `python bcstoremaker.py`. You will be prompted for a category of products to create. **This category will be created in your store if it does not exist**
 5. Enter any extra keywords you want GPT to consider when creating your products
 6. Enter how many products you want created
-7. Wait for the script to run.
+7. Enter how many users and orders you want created
+8. Wait for the script to run.
 
-## Screenshots
-![](img/20231011200424.png)
-
-![](img/20231011200447.png)
 
 ## Cost
 Doing some rough napkin math, generating 1000 products with 512x512 images should cost you about $27 ($9 for GPT-4 and $18 for 1000 dalle images). The $9 could be reduced some by switching to gpt-3 or another cheaper model. 
+
+
+## Screenshots
+
+![](img/20231012080348.png)
+
+
+
+![](img/20231011200447.png)
+
+![](img/20231012080430.png)
+
+## Todos
+* Status IDs selection
+* Make more efficient by asking for batches of products from GPT
+* Support category nesting
+* Support multiple categories at once
+* Ask GPT to edit store settings? 
--- bcstoremaker.py
+++ bcstoremaker.py
@@ -1,9 +1,10 @@
-# Import the required packages and libraries
 import openai
 import bigcommerce
 import requests
+import random
+import time
+import datetime
 
-# Create a configuration file that stores the API keys and store hash
 config = {
     "openai_api_key": "sk-xxx",
     "bigcommerce_api_key": "xxx",
@@ -11,22 +12,19 @@ config = {
     "bigcommerce_client_id": "xxx"
 }
 
-# Connect to the BigCommerce API using OAuth
 api = bigcommerce.api.BigcommerceApi(
     client_id=config["bigcommerce_client_id"],
     store_hash=config["bigcommerce_store_hash"],
     access_token=config["bigcommerce_api_key"]
 )
 
-# Connect to the OpenAI API using your secret key
 openai.api_key = config["openai_api_key"]
 
-# Write a function that takes a product category name and other inputs from the user and uses the ChatGPT API to generate a product name, description, price, and weight
 def generate_product(category, inputs):
-    # Use the ChatCompletion endpoint with the gpt-4 model to get natural language responses from ChatGPT
+
     query = f"Generate a product name: , description: , price: , and weight: for a {category} product based on these inputs: {inputs}\n\nExample output:\nProduct name: Alpine Explorer Men's Outdoor Jacket\nDescription: Crafted for those who love the great outdoors, the Alpine Explorer Men's Outdoor Jacket is a stylish fusion of form and function. Made with a blend of high-performance materials, it ensures optimal warmth and water-resistance while providing full-range mobility.\nPrice: 249.99\nWeight: 2.5\n\nPlease only output in this format. Make sure to not include dollar signs on the prices."
     try:
-        # Add a system message to provide some context and instructions to the model
+
         response = openai.ChatCompletion.create(
             messages=[
                 {"role": "system", "content": "You are a creative assistant who can generate products for any category based on some inputs."},
@@ -37,25 +35,25 @@ def generate_product(category, inputs):
     except openai.error.OpenAIError as e:
         print(f"An error occurred while using the OpenAI API: {e}")
         return None
-    # Parse the response and return a dictionary with the product data
+
     product_data = {}
-    # Check if the response object has the choices attribute
+
     if hasattr(response, "choices"):
-        # Get the list of choices from the response
+
         choices = response.choices
-        # Filter out any choices that have an incomplete or invalid finish reason
+
         valid_choices = [choice for choice in choices if choice.finish_reason in ["stop", "length"]]
-        # Choose one of the valid choices, either by using the first one or by using some criteria
-        chosen_choice = valid_choices[0] # This is a simple example, you can use other criteria to choose the best choice
-        # Get the message from the chosen choice
+
+        chosen_choice = valid_choices[0] 
+
         message = chosen_choice.message
-        # Check if the message role is assistant
+
         if message.role == "assistant":
-            # Split the message content by newline characters to get the lines
+
             lines = message.content.split("\n")
-            # Check if the lines have the expected format and length of 4
+
             if len(lines) == 4 and all(line.startswith(("Product name", "Description", "Price", "Weight")) for line in lines):
-                # Extract the product data from each line by splitting by colon and stripping whitespace
+
                 product_data["name"] = lines[0].split(":")[1].strip()
                 product_data["description"] = lines[1].split(":")[1].strip()
                 try:
@@ -70,22 +68,20 @@ def generate_product(category, inputs):
                     return None
                 return product_data
             else:
-                # The lines do not have the expected format or length, so return None
+
                 print(f"The response from the OpenAI API was not valid: {message.content}")
                 return None
         else:
-            # The message role is not assistant, so return None
+
             print(f"The response from the OpenAI API was not from an assistant: {message.content}")
             return None
     else:
-        # The response object does not have the choices attribute, so return None
+
         print(f"The response from the OpenAI API was empty or invalid: {response}")
         return None
-
-# Write a function that takes a category name as an input and returns the category ID as an output
 def get_or_create_category(category):
     # Get a list of all the categories and their IDs using the BigCommerce API
-    categories = api.Categories.all()
+    categories = api.Categories.iterall()
     # Loop through the categories and compare the names with the input
     for cat in categories:
         # If there is a match, return the ID
@@ -95,7 +91,6 @@ def get_or_create_category(category):
     new_category = api.Categories.create(name=category)
     return new_category.id
 
-# Write a function that takes the product data generated by ChatGPT and uses the BigCommerce API to create a product on your BigCommerce store
 def create_product(product_data, category):
     # Create a product object with the required fields using the BigCommerce API Python Client
     try: # Added a try-except block to handle any errors from the BigCommerce API
@@ -114,98 +109,275 @@ def create_product(product_data, category):
     except bigcommerce.exception.ClientRequestException as e: # Added an exception handler for ClientRequestException, which is raised when there is an error in the request parameters or headers
         print(f"An error occurred while creating the product on BigCommerce: {e}")
         return None
-
-# Write a function that takes a product description and uses the OpenAI API to generate an image URL from it
+    
 def generate_image(description):
-    # Use the Image.create method to generate an image from the description using DALL-E
-    try: # Added a try-except block to handle any errors from the OpenAI API
+
+    try: 
         image = openai.Image.create(
             prompt=description,
-            size="512x512", # Set the size of the image in pixels (width, height)
+            size="512x512", 
         )
-        # Return the URL of the image
-        return image.data[0].url # Get the URL from the first element of the data list
-    except openai.error.OpenAIError as e: # Added an exception handler for OpenAIError, which is raised when there is an error in the OpenAI API request or response
+
+        return image.data[0].url 
+    except openai.error.OpenAIError as e: 
         print(f"An error occurred while generating an image from OpenAI: {e}")
         return None
 
 def create_image(image_url, product_id):
-    # Use the requests library to download the image data from the URL
+
     response = requests.get(image_url)
-    if response.status_code == 200: # Check if the response is successful (200 OK)
-        # Use the Images.create method to create an image on BigCommerce using the product ID and the image data
-        
-        try: # Added a try-except block to handle any errors from the BigCommerce API
-            # Create a payload dictionary with the image file object
+    if response.status_code == 200: 
+
+        try: 
+
             payload = {
-                'image_file': ('image.jpg', response.content, 'image/jpeg') # Set this to a tuple of (filename, file content, file type)
+                'image_file': ('image.jpg', response.content, 'image/jpeg') 
             }
-            
-            # Create a header dictionary with the authorization
+
             headers = {
                 'X-Auth-Client': config["bigcommerce_client_id"],
                 'X-Auth-Token': config["bigcommerce_api_key"],
             }
-            
-            # Send a POST request to the v3 catalog API endpoint for creating an image
+
             response = requests.post(f'https://api.bigcommerce.com/stores/{config["bigcommerce_store_hash"]}/v3/catalog/products/{product_id}/images', files=payload, headers=headers)
-            
-            # Check the response status code and print the result
-            if response.status_code == 200: # The request was successful
+
+            if response.status_code == 200: 
                 print(f"Image created successfully!")
-            else: # The request failed
+            else: 
                 print(f"An error occurred while creating an image on BigCommerce: {response.status_code} {response.text}")
-        except bigcommerce.exception.ClientRequestException as e: # Added an exception handler for ClientRequestException, which is raised when there is an error in the request parameters or headers
+        except bigcommerce.exception.ClientRequestException as e: 
             print(f"An error occurred while creating an image on BigCommerce: {e}")
             return None
-    else: # The response is not successful, so return None
+    else: 
         print(f"An error occurred while downloading the image from OpenAI: {response.status_code}")
         return None
 
+def generate_user():
 
-# Write a main function that initializes the application and prompts the user for inputs and calls the functions to generate and create products
-def main():
-    # Initialize the application and read the configuration file
-    print("Welcome to the BigCommerce Product Generator!")
-    print("This application will help you create products on your BigCommerce store using the ChatGPT and DALL-E APIs.")
+    user_data = {}
+
+    user_data["name"] = random.choice(["John", "Jane", "Jack", "Jill", "James", "Jenny", "Jeff", "Julia", "Jake", "Jess"]) + " " + random.choice(["Smith", "Jones", "Brown", "Wilson", "Miller", "Davis", "Taylor", "Clark", "Lee", "White"])
+    user_data["email"] = user_data["name"].lower().replace(" ", ".") + str(random.randint(1,1000)) + "@example.com"
+
+    return user_data
+
+def create_user(user_data):
+
+    try: 
+        user = api.Customers.create(
+            first_name=user_data["name"].split()[0],
+            last_name=user_data["name"].split()[1],
+            email=user_data["email"]
+        )
+
+        return user
+    except bigcommerce.exception.ClientRequestException     as e: 
+        print(f"An error occurred while creating the user on BigCommerce: {e}")
+        return None
+
+
+
+def generate_order(user_id, product_ids):
+
+    order_data = {}
+
+    # Use a static list of order status IDs instead of polling from the API
+    order_status_ids = [2, 9, 10]
+    order_data["status_id"] = random.choice(order_status_ids)
+
+    # Use today's date plus or minus a few weeks as the order date
+    today = datetime.date.today()
+    delta = datetime.timedelta(days=random.randint(-21, 21))
+    order_date = today + delta
+
+    # Format the order date as a valid RFC-2822 date using the email.utils module
+    import email.utils
+    # Convert the order date to a time.struct_time object using the timetuple method
+    order_date_struct = order_date.timetuple()
+    # Convert the time.struct_time object to a float representing seconds since epoch using the time.mktime function
+    order_date_seconds = time.mktime(order_date_struct)
+    # Format the float as a valid RFC-2822 date using the email.utils.formatdate function
+    order_date_rfc = email.utils.formatdate(timeval=order_date_seconds, localtime=False, usegmt=True)
+    order_data["date_created"] = order_date_rfc
+
+    # Remove the prices and product names from the order data
+    products = []
+    num_products = random.randint(1, len(product_ids))
+    for i in range(num_products):
+        product_id = random.choice(product_ids)
+        quantity = random.randint(1, 5)
+        products.append({
+            "product_id": product_id,
+            "quantity": quantity
+        })
+
+    # Remove the tax amount from the order data
+    order_data["shipping_cost"] = random.choice([0, 5.99, 9.99, 14.99])
+
+    # Generate a billing address for the order using the user's name and email
+    user = api.Customers.all(min_id=user_id)[0]
+    first_name, last_name = user.first_name, user.last_name
+    email = user.email
+
+    # Use a dictionary of cities and states that go together instead of selecting them randomly
+    city_state_dict = {
+        "Austin": "Texas",
+        "Boston": "Massachusetts",
+        "Chicago": "Illinois",
+        "Denver": "Colorado",
+        "New York": "New York"
+    }
+    city, state = random.choice(list(city_state_dict.items()))
     
-    # Prompt the user for a product category name and other inputs
+    street_1 = random.choice(["123 Main Street", "456 Elm Street", "789 Pine Street"])
+    zip_code = random.randint(10000, 99999)
+    country = "United States"
+    country_iso2 = "US"
+    
+    billing_address = {
+        "first_name": first_name,
+        "last_name": last_name,
+        "company": "",
+        "street_1": street_1,
+        "street_2": "",
+        "city": city,
+        "state": state,
+        "zip": zip_code,
+        "country": country,
+        "country_iso2": country_iso2,
+        "phone": "",
+        "email": email
+    }
+
+    # Add the billing address to the order data
+    order_data["billing_address"] = billing_address
+
+    return order_data, products
+
+    # Add the billing address to the order data
+    order_data["billing_address"] = billing_address
+
+    return order_data, products
+
+def create_order(order_data, user_id, products):
+
+    try:
+        # Use the api.Orders.create method instead of the create_from_dict method
+        order = api.Orders.create(
+            customer_id=user_id,
+            date_created=order_data["date_created"],
+            status_id=order_data["status_id"],
+            products=products,
+            billing_address=order_data["billing_address"]
+        )
+
+        return order
+    except bigcommerce.exception.ClientRequestException as e: 
+        print(f"An error occurred while creating the order on BigCommerce: {e}")
+        return None
+
+
+def main():
+
+    print("Welcome to the BigCommerce Product Generator!")
+    print("This application will help you create products, users, and orders on your BigCommerce store using the ChatGPT and DALL-E APIs.")
+
     category = input("Please enter a product category name: ")
     inputs = input("Please enter any other inputs you want to use for generating products (separated by commas): ")
-    # Prompt the user for the number of products to generate and create
-    num_products = int(input("Please enter the number of products you want to generate and create: "))
 
-    # Loop through the number of products and call the functions to generate and create products
+    num_products = int(input("Please enter the number of products you want to generate and create: "))
+    num_users = int(input("Please enter the number of users you want to generate and create: "))
+    num_orders_per_user = int(input("Please enter the number of orders per user you want to generate and create: "))
+
+    product_ids = []
     for i in range(num_products):
         print(f"Generating product {i+1}...")
-        # Generate a product using ChatGPT
+
         product_data = generate_product(category, inputs)
-        if product_data: # Added a check for None product data to avoid passing it to the next function
-            # Create a product using BigCommerce
+        if product_data: 
+
             product = create_product(product_data, category)
-            if product: # Added a check for None product object to avoid printing invalid data
-                # Print the result
+            if product: 
+
                 print(f"Product {i+1} created successfully!")
                 print(f"Product ID: {product.id}")
                 print(f"Product Name: {product.name}")
                 print(f"Product Description: {product.description}")
                 print(f"Product Price: {product.price}")
                 print(f"Product Weight: {product.weight}")
-                # Generate an image URL using DALL-E
+
                 image_url = generate_image(product.description)
-                if image_url: # Added a check for None image URL to avoid passing it to the next function
-                    # Create an image on BigCommerce using the URL
+                if image_url: 
+
                     image = create_image(image_url, product.id)
-                    if image: # Added a check for None image object to avoid printing invalid data
-                        # Print the result
+                    if image: 
+
                         print(f"Image created successfully!")
-                       
-                    
+
                 else:
                     print(f"Image generation failed!")
+
+                product_ids.append(product.id)
+
             else:
                 print(f"Product {i+1} creation failed!")
         else:
             print(f"Product {i+1} generation failed!")
-        print("Thank you for using the BigCommerce Product Generator!")
+
+    user_ids = []
+    for i in range(num_users):
+        print(f"Generating user {i+1}...")
+
+        user_data = generate_user()
+        if user_data:
+
+            user = create_user(user_data)
+            if user:
+
+                print(f"User {i+1} created successfully!")
+                print(f"User ID: {user.id}")
+                print(f"User Name: {user.first_name} {user.last_name}")
+                print(f"User Email: {user.email}")
+
+                user_ids.append(user.id)
+
+            else:
+                print(f"User {i+1} creation failed!")
+        else:
+            print(f"User {i+1} generation failed!")
+
+    order_ids = []
+    for user_id in user_ids:
+        for i in range(num_orders_per_user):
+            print(f"Generating order {i+1} for user {user_id}...")
+
+            order_data, products = generate_order(user_id, product_ids)
+            if order_data and products:
+
+                order = create_order(order_data, user_id, products)
+                if order:
+
+                    print(f"Order {i+1} for user {user_id} created successfully!")
+                    print(f"Order ID: {order.id}")
+                    print(f"Order Date: {order.date_created}")
+                    print(f"Order Status: {order.status_id}")
+                    print(f"Order Subtotal: {order.subtotal_inc_tax}")
+                    print(f"Order Shipping Cost: {order.shipping_cost_inc_tax}")
+                    print(f"Order Tax Amount: {order.total_tax}")
+                    print(f"Order Total: {order.total_inc_tax}")
+                    print(f"Order Products:")
+                    for product in order.products():
+                        print(f"- Product ID: {product.product_id}")
+                        print(f"- Product Name: {product.name}")
+                        print(f"- Product Quantity: {product.quantity}")
+                        print(f"- Product Price: {product.price_inc_tax}")
+
+                    order_ids.append(order.id)
+
+                else:
+                    print(f"Order {i+1} for user {user_id} creation failed!")
+            else:
+                print(f"Order {i+1} for user {user_id} generation failed!")
+
+    print("Thank you for using the BigCommerce Product Generator!")
 main()
